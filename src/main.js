9bb012f478457fced75ce9e5b9f6b2708de3d2ba
analizanaya <lizinayari@gmail.com>
2023-02-21 07:16:46 -0500

    prueba1
    Co-authored-by: mijitanita <mijitanita@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,28 +1,16 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 // console.log(pokemon.pokemon)
-let acortado = data.pokemon.slice(0,9);
-console.log(acortado)
+const acortado = data.pokemon.slice(0,9);
+//console.log(acortado)
 
 function mostrarPokemon(arreglo){
-    const contenedorPokemon = document.getElementById('contenedorPokemon')
-    for(let i = 0 ; i < arreglo.length ; i++ ){
-        // crear elementos *****
-      //  console.log(arreglo[i])
-     /* const h1 = document.createElement('h1')
-      h1.textContent = arreglo[i].name
-      contenedorPokemon.appendChild(h1) */
-      // interpolacion de variables y template string *****
-      /*
-       contenedorPokemon.innerHTML += ` <article>
-       <h4>${arreglo[i].name}</h4>
-       <img src="${arreglo[i].img}" alt="">
-     </article>`*/
-     //concatenacion ****
-     // contenedorPokemon.innerHTML += '<h4>' + arreglo[i].name + '</h4>'
-
-     contenedorPokemon.innerHTML += ` <article>
+  const contenedorPokemon = document.getElementById('contenedorPokemon')
+  for(let i = 0 ; i < arreglo.length ; i++ ){
+ 
+    contenedorPokemon.innerHTML += 
+  `<div class="cardImage">
        <h5>${arreglo[i].num}</h5>
        <h1>${arreglo[i].name}</h1>
        <h5></h5> Num: ${arreglo[i].generation.num}</h5>
@@ -35,16 +23,8 @@ function mostrarPokemon(arreglo){
       <h4>${arreglo[i].type}</h4>
       <h3> RESISTANT ${arreglo[i].resistant}</h3>
       <h3> WEAKNESSES ${arreglo[i].weaknesses}</h3>
-      <h6> <img src="/egg.png">${arreglo[i].egg}</h6>
-      <h6> <img src="/candy.png">${arreglo[i].evolution.candy}</h6>
-
-
-      
-
-
-
-     </article>`
-     
-    }
+      <h6> <img src="/egg-01.png" class="huevo">${arreglo[i].egg}</h6>
+      <h6> <img src="/candy-02.png" class="caramelo">${arreglo[i].evolution.candy}</h6>
+      </div>` }
 }
 mostrarPokemon(acortado)
