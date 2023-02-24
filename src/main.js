ebd653fc446d5d2ae2b62f9234601f5e0e286a1f
analizanaya <lizinayari@gmail.com>
2023-02-23 19:50:26 -0500

    css de tarjeta 2

--- src/main.js
+++ src/main.js
@@ -8,24 +8,28 @@ const acortado = data.pokemon.slice(0,9);
 function mostrarPokemon(arreglo){
   const contenedorPokemon = document.getElementById('contenedorPokemon')
   for(let i = 0 ; i < arreglo.length ; i++ ){
- 
+  
     contenedorPokemon.innerHTML += 
   `<section class="cardImage">
   <section class="rectanguloInterno">
        <h5 class="num">${arreglo[i].num}</h5>
-       <h1>${arreglo[i].name}</h1>
-       <h5> Num: ${arreglo[i].generation.num}</h5>
-       <h5> Name: ${arreglo[i].generation.name}</h5>
-       <h4> CP: ${arreglo[i].stats ["max-cp"]}</h4>
-       <h4> HP: ${arreglo[i].stats ["max-hp"]}</h4>
-       <img src="${arreglo[i].img}" alt="">
-       <h6> Height: ${arreglo[i].size.height}</h6>
-       <h6> Weight: ${arreglo[i].size.weight}</h6>
-      <h4>${arreglo[i].type}</h4>
-      <h3> RESISTANT ${arreglo[i].resistant}</h3>
-      <h3> WEAKNESSES ${arreglo[i].weaknesses}</h3>
-      <h6> <img src="/egg-01.png" class="huevo">${arreglo[i].egg}</h6>
-      <h6> <img src="/candy-02.png" class="caramelo">${arreglo[i].evolution.candy}</h6>
+       <h1 class="nombre">${arreglo[i].name}</h1>
+       <h5 class="generacionNumero"> <span class="negrita">Num: </span>${arreglo[i].generation.num}</h5>
+       <h5 class="generacionNombre"> <span class="negrita">Name:</span>${arreglo[i].generation.name}</h5>
+       <h4 class="cp"> CP: ${arreglo[i].stats ["max-cp"]}</h4>
+       <h4 class="hp"> HP: ${arreglo[i].stats ["max-hp"]}</h4>
+       <img src="${arreglo[i].img}" alt="" class="imagen">
+       <h6 class="altura"> <span class="negrita">Height: </span> ${arreglo[i].size.height}</h6>
+       <h6 class="peso"> <span class="negrita">Weight:</span> ${arreglo[i].size.weight}</h6>
+      <h4 class="tipo">${arreglo[i].type}</h4>
+      <h5 class="palabraResistant">RESISTANT</h5>
+      <h3 class="resistencia"> ${arreglo[i].resistant}</h3>
+      <h5 class="palabraDebilidad">WEAKNESSES</h5>
+      <h3 class="debilidad">${arreglo[i].weaknesses}</h3>
+      <h6> <img src="./imagenes/egg-01.png" class="imagenHuevo"></h6>
+      <h6 class="huevo">${arreglo[i].egg}</h6>
+      <h6> <img src="./imagenes/candy-02.png" class="caramelo"></h6>
+      <h6 class="infoCaramelo">${arreglo[i].evolution.candy}</h6>
       </section>
       </section>`}
 }
